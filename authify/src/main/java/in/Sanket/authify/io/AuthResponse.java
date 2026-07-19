34b7e86f2913c030483830b6116fed86c814efc4
Sanket <sanketkumar7645@gmail.com>
2026-07-19 18:38:24 +0530

    Re-Uploaded

---
+++ authify/src/main/java/in/Sanket/authify/io/AuthResponse.java
@@ -0,0 +1,12 @@
+package in.Sanket.authify.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse
+{
+    private String email;
+    private String token;
+}
